81c24f6c74ba0e097af37168171df4913335ede3
RiveSunder <rivesunder@protonmail.com>
2022-08-18 16:16:25 -0600

    add outline for metrics functions
    name changed from 'get' to 'compute' to more accurately reflect what those functions do

--- symr/metrics.py
+++ symr/metrics.py
@@ -1,12 +1,12 @@
 import sympy as sp
 import numpy as np
 
-def get_complexity(expression):
+def compute_complexity(expression):
         
     tree_nodes = [element for element in sp.preorder_traversal(sp.simplify(expression))]
     return len(tree_nodes)
 
-def get_shannon_diversity(expression):
+def compute_shannon_diversity(expression):
     # H = -Σpi * ln(pi)    
 
     simple = sp.simplify(expression)
@@ -20,3 +20,70 @@ def get_shannon_diversity(expression):
 
     return h            
        
+
+def compute_exact_equivalence(expression_a, expression_b):
+    """
+    symbolic metric
+
+    reported in:
+        Petersen _et al._ 2019
+        La Cava _et al._ 2021
+    """
+    pass
+
+def compute_tree_distance(expression_a, expression_b):
+    """
+    symbolic metric
+    
+    reported in:
+        
+    """
+    pass
+    
+
+def compute_r2(expression_a, expression_b, inputs):
+    """
+    numerical metric
+    
+    """
+    pass
+
+def compute_truncated_r2(expression_a, expression_b, inputs):
+    """
+    numerical metric
+    
+    reported in:
+        Kamienny _et al._ 2022
+    """
+    pass
+
+def compute_relative_error(expression_a, expression_b, inputs):
+    """
+    numerical metric
+
+    relative absolute error
+
+    reported in:
+        Vastl _et al._ 2022
+    """
+    pass
+    
+def compute_isclose(expression_a, expression_b, inputs): 
+    """
+    numerical metric (ad-hoc accuracy proxy)
+
+    reported in:
+        Biggio _et al._ 2021
+    """
+    pass
+
+
+def compute_r2_over_threshold(expression_a, expression_b, inputs, threshold):
+    """
+    numerical metric (ad-hoc accuracy proxy)
+
+    reported in:
+        Kamienny _et al._ 2022 (with threshold 0.99)
+        Biggio _et al._ 2019 (with threshold 0.95)
+    """
+    pass
