6c89559c79381c05a8bd99338137b90164665f32
RiveSunder <rivesunder@protonmail.com>
2022-08-18 16:28:20 -0600

    add test for compute_tree_distance

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(\
         name="symr", \
-        packages=["symr"], \
+        packages=["symr", "symr_tests"], \
         version = "0.0", \
         description = "The Metric is the Massage", \
         install_requires=[\
---
+++ symr_tests/symr/test_metrics.py
@@ -0,0 +1,58 @@
+import os
+
+import unittest
+
+import numpy as np
+import sympy as sp
+
+from symr.metrics import compute_tree_distance
+
+class TestComputeTreeDistance(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+    def test_compute_tree_distance(self):
+
+        expression_a = "x**2 + y**2 + 10"
+        expression_b = "x**2 + y**3 + 10"
+        expression_c = "x**3 + y**3 + 10"
+        expression_d = "sin(x)**2 + 1/y**2 + 10"
+
+        dist_aa = compute_tree_distance(expression_a, expression_b)
+        dist_ab = compute_tree_distance(expression_a, expression_b)
+        dist_ac = compute_tree_distance(expression_a, expression_b)
+        dist_ad = compute_tree_distance(expression_a, expression_b)
+        dist_ba = compute_tree_distance(expression_a, expression_b)
+        dist_bb = compute_tree_distance(expression_a, expression_b)
+        dist_bc = compute_tree_distance(expression_a, expression_b)
+        dist_bd = compute_tree_distance(expression_a, expression_b)
+        dist_ca = compute_tree_distance(expression_a, expression_b)
+        dist_cb = compute_tree_distance(expression_a, expression_b)
+        dist_cc = compute_tree_distance(expression_a, expression_b)
+        dist_cd = compute_tree_distance(expression_a, expression_b)
+        dist_da = compute_tree_distance(expression_a, expression_b)
+        dist_db = compute_tree_distance(expression_a, expression_b)
+        dist_dc = compute_tree_distance(expression_a, expression_b)
+        dist_dd = compute_tree_distance(expression_a, expression_b)
+
+        self.assertEqual(dist_aa, 0)
+        self.assertEqual(dist_bb, 0)
+        self.assertEqual(dist_cc, 0)
+        self.assertEqual(dist_dd, 0)
+
+        self.assertEqual(dist_ab, dist_ba)
+        self.assertEqual(dist_ac, dist_ca)
+        self.assertEqual(dist_ad, dist_da)
+
+        self.assertEqual(dist_bc, dist_cb)
+        self.assertEqual(dist_bd, dist_db)
+
+        self.assertEqual(dist_cd, dist_dc)
+
+        self.assertEqual(dist_ab, 1)
+        self.assertEqual(dist_ac, 2)
+
+if __name__ == "__main__":
+
+    unittest.main(verbosity=2)
