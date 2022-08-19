88ed884ac68dcb2fc2acb49c03780c0863e92120
RiveSunder <rivesunder@protonmail.com>
2022-08-19 11:36:13 -0600

    add test_all for running all tests

---
+++ symr_tests/test_all.py
@@ -0,0 +1,12 @@
+import os
+
+import unittest
+
+import numpy as np
+import sympy as sp
+
+from symr_tests.symr.test_metrics import TestExactEquivalence, TestComputeTreeDistance
+
+if __name__ == "__main__":
+
+    unittest.main(verbosity=2)
