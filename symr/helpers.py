4bf374c48d34864bf8408e1054849e11c9ef0034
RiveSunder <rivesunder@protonmail.com>
2022-09-22 11:15:18 -0600

    change default data to load to nguyen_test.csv, should speed up tests

--- symr/helpers.py
+++ symr/helpers.py
@@ -3,7 +3,7 @@ import os
 def load_benchmark(filepath="data/nguyen.csv"):
 
     if filepath is None:
-        filepath="data/nguyen.csv"
+        filepath="data/nguyen_test.csv"
 
     with open(filepath, "r") as f:
         lines = f.readlines()
