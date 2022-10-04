d09546713c482b5b35980b13144097bb28372046
RiveSunder <rivesunder@protonmail.com>
2022-10-04 10:13:21 -0600

    add r2 over threshold curve and area under the curve helpers

--- symr/helpers.py
+++ symr/helpers.py
@@ -1,5 +1,7 @@
 import os
 
+from functools import reduce
+
 def load_benchmark(filepath="data/nguyen.csv"):
 
     if filepath is None:
@@ -10,4 +12,20 @@ def load_benchmark(filepath="data/nguyen.csv"):
 
 
     return lines
+
+
+def r2_over_threshold(r2, thesholds=None):
+
+    if thresholds is None:
+        thresh = np.arange(0,1.0,0.01)
+
+    r2_ot = [(x > threshold).sum() / x.shape[0] for threshold in thresh ]
+
+    return r2_ot
+
+def r2_auc(roc_curve):
+
+    raw_result = reduce(lambda a,b: a+b, roc_curve)
+
+    return raw_result / len(roc_curve)
 
